1da9de8e675a91bb90067a0a2346c88411418717
Tom Wilson <twilson90@hotmail.co.uk>
2021-10-14 11:14:42 +0100

    this should work

--- filters/f_decoder_wrapper.c
+++ filters/f_decoder_wrapper.c
@@ -662,19 +662,6 @@ static void crazy_video_pts_stuff(struct priv *p, struct mp_image *mpi)
         mpi->pts == MP_NOPTS_VALUE) && mpi->dts != MP_NOPTS_VALUE) {
         mpi->pts = mpi->dts;
     }
-    
-    double pts_delta = mpi->pts - p->pts;
-    double base = p->first_packet_pdts;
-    double fps = p->fps > 0 ? p->fps : 25;
-    double frame_time = 1.0f / fps;
-    if (mpi->pts == MP_NOPTS_VALUE) {
-        mpi->pts = base == MP_NOPTS_VALUE ? 0 : base;
-    } else if (pts_delta < 0.0001) {
-        mpi->pts = p->pts + frame_time;
-        MP_WARN(p, "Correcting Video PTS "
-                    "%f -> %f.\n", p->pts, mpi->pts);
-    }
-
     // Compensate for incorrectly using mpeg-style DTS for avi timestamps.
     if (p->decoder && p->decoder->control && p->codec->avi_dts &&
         mpi->pts != MP_NOPTS_VALUE && p->fps > 0)
@@ -747,8 +734,10 @@ static void correct_video_pts(struct priv *p, struct mp_image *mpi)
 {
     mpi->pts *= p->play_dir;
 
+    double fps = p->fps > 0 ? p->fps : 25;
+    double frame_time = 1.0f / fps;
+    
     if (!p->opts->correct_pts || mpi->pts == MP_NOPTS_VALUE) {
-        double fps = p->fps > 0 ? p->fps : 25;
 
         if (p->opts->correct_pts) {
             if (p->has_broken_decoded_pts <= 1) {
@@ -760,13 +749,19 @@ static void correct_video_pts(struct priv *p, struct mp_image *mpi)
             }
         }
 
-        double frame_time = 1.0f / fps;
         double base = p->first_packet_pdts;
         mpi->pts = p->pts;
         if (mpi->pts == MP_NOPTS_VALUE) {
             mpi->pts = base == MP_NOPTS_VALUE ? 0 : base;
         } else {
             mpi->pts += frame_time;
+        }
+    } else {
+        double frame_time = 1.0f / fps;
+        double pts_delta = mpi->pts - p->pts;
+        if (pts_delta < 0.0001) {
+            mpi->pts = p->pts + frame_time;
+            MP_WARN(p, "Correcting Video PTS %f -> %f.\n", p->pts, mpi->pts);
         }
     }
 
